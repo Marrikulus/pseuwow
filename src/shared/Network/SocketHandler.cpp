dfcc8e222389eb24c699aed98a610a0fbce43ce6
Ingimar Svanberg Jóhannesson <ingimar.johannesson@gmail.com>
2019-01-14 23:50:48 +0000

    added a missing header

--- src/shared/Network/SocketHandler.cpp
+++ src/shared/Network/SocketHandler.cpp
@@ -25,6 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #pragma warning(disable:4786)
 #include <stdlib.h>
 #else
+#include <stdlib.h>
 #include <errno.h>
 #endif
 
